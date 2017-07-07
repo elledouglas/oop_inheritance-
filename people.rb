847f2ec0c91d18a06041c03b9e98ff62a81c25d6
Kathleen <kathleenmcclay@gmail.com>
2017-07-07 10:30:59 -0400

    Added to people file

--- people.rb
+++ people.rb
@@ -1,14 +1,16 @@
 class Person
 
-  def initialize
-    @name = name
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+
   end
   def reader
     name
   end
 
-  def greeting(name)
-puts "Hi my name is #{@name}"
+  def greeting
+puts "Hi my name is #{@first_name} #{@last_name}"
 
   end
 end
@@ -16,15 +18,22 @@ end
 
 
 class Student < Person
+  attr_reader :knowledge_level
+
+  #when a student learns, the knowledge_level goes up 
   def learn
     puts "I get it"
   end
 end
 
 class Instructor < Person
+
+  attr_accessor :field_of_expertise
+
   def teach
     puts "Everything in Ruby is an Object"
   end
+
 
 end
 
