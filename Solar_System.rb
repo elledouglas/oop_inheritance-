2d6b9c0bba49ba3c03dc6ff5319adadbf6de19f2
Kathleen <kathleenmcclay@gmail.com>
2017-07-07 06:50:15 -0400

    Solar_System framework complete

---
+++ Solar_System.rb
@@ -0,0 +1,54 @@
+class System
+
+def initialize
+  @bodies = []
+end
+
+  def reader
+@bodies
+    end
+
+def add
+end
+
+def total_mess
+@bodies.each do |body|
+  body = body + 1
+  return sum
+end
+end
+
+class Body
+def initialize
+@name = name
+@mass = mass
+
+def reader
+  @name, @mass
+end
+end
+end
+
+class Planets
+  def day
+  end
+
+  def year
+  end
+end
+
+class Star
+
+  def type
+  end
+end
+
+class Moon
+  def month
+  end
+  def planet
+    Planets
+  end
+end
+
+end
